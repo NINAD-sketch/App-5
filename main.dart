4032a365e439fc167f7db0d1d07e64c36958e4ab
Ninad Kuralkar <ninad.kuralkar2@gmail.com>
2025-09-05 02:03:20 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,21 @@
+import 'package:assignment_5/home_page.dart';
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.transparent,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
